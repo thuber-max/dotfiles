92d55af47e16ce38f259db1cfb8c05f5df8c25d5
Travis Huber <travis@innovativeorange.com>
2015-01-26 22:53:29 -0600

    add a setup script

---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  setup.sh
+# 
+#         USAGE:  ./setup.sh 
+# 
+#   DESCRIPTION:  Setup my dotfiles on a new machine
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:   (), 
+#       COMPANY:  
+#       VERSION:  1.0
+#       CREATED:  01/26/2015 22:42:19 CST
+#      REVISION:  ---
+#===============================================================================
+
+STARTDIR=`pwd`
+
+# The path to the directory containing this file
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+# set up my dir colors
+cat ${SCRIPTPATH}/dircolors >> ~/.dircolors
+
+# set up my bash file
+cat ${SCRIPTPATH}/bashrc >> ~/.bashrc
+
+# setup vim
+git clone https://github.com/thuber-max/vim-ide.git ~/.vim && \
+cd ~/.vim && \
+bash ~/.vim/setup_vim.sh
+
+source ~/.bashrc
+
+cd ${STARTDIR}
